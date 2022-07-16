e85820aff5f2b4e7cba2284eb071d864cf74656c
umidjonotaqulov <otaqulovumidjon2003@gmail.com>
2022-07-16 08:31:24 +0500

    13

--- basic_func13.py
+++ basic_func13.py
@@ -1,3 +1,13 @@
 # Create a function called main.
 # Create function argument a.
 # Returns the square root of a number.
+
+from math import sqrt
+
+def main(a):
+
+    return sqrt(a)
+
+x=main(49)
+
+print(x)

1893fa9f9420fa1000f9e672ec9e230b288a2b8b
umidjonotaqulov <otaqulovumidjon2003@gmail.com>
2022-07-16 08:44:08 +0500

    19

--- basic_func19.py
+++ basic_func19.py
@@ -2,9 +2,13 @@
 # Create function arguments a and b.
 # Return the absolute value of the difference between a and b.
 
+# Asosiy deb nomlangan funksiya yarating.
+# a va b funktsiya argumentlarini yarating.
+# a va b o'rtasidagi farqning mutlaq qiymatini qaytaring.
+
 def main(a,b):
 
-    return a-b
+    return abs(a-b)
 
 x=main(43,20)
 

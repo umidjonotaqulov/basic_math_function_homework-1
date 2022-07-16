0316a599a6ec5cb46a99452ed9fc2b8b21bf601c
umidjonotaqulov <otaqulovumidjon2003@gmail.com>
2022-07-16 08:40:59 +0500

    17

--- basic_func17.py
+++ basic_func17.py
@@ -2,6 +2,10 @@
 # Create function arguments a.
 # Assign the value pi to the variable "a" and return.
 
+# Asosiy deb nomlangan funksiya yarating.
+# Funktsiya argumentlarini yaratish a.
+# "a" o'zgaruvchisiga pi qiymatini belgilang va qaytaring.
+
 from math import pi
 
 def main(a):

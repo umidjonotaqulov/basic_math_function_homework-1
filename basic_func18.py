2607988fce097df0ac25086a1039459c09f19373
umidjonotaqulov <otaqulovumidjon2003@gmail.com>
2022-07-16 12:32:49 +0500

    18

--- basic_func18.py
+++ basic_func18.py
@@ -3,10 +3,20 @@
 # Round the result to 2 decimal places.
 # Assign the value pi to the variable "a" and return.
 
+# Asosiy deb nomlangan funksiya yarating.
+# Funktsiya argumentlarini yaratish a.
+# Natijani 2 kasrgacha yaxlitlang.
+# "a" o'zgaruvchisiga pi qiymatini belgilang va qaytaring.
+
+from math import pi
+
 def main(a):
+
+    a=pi
 
     return round(a,2)
 
-x=main(3.14)
+x=main(0)
 
 print(x)
+    

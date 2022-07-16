bc0d934b61a2244102b616e286e5c95e05aaf5ed
umidjonotaqulov <otaqulovumidjon2003@gmail.com>
2022-07-16 08:37:29 +0500

    20

--- basic_func20.py
+++ basic_func20.py
@@ -1,3 +1,15 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the multiplication of a and b.
+
+# Asosiy deb nomlangan funksiya yarating.
+# a va b funktsiya argumentlarini yarating.
+# a va b ning ko'paytmasini qaytaring.
+
+def main(a,b):
+
+    return a*b
+
+x=main(65,20)
+
+print(x)
